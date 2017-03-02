db5799677b73e62e230948e7542cb819d8899a41
Timur Morozov <radist@desant.biz>
2017-03-02 14:22:16 +0200

    use function components

--- src/pages/about/index.js
+++ src/pages/about/index.js
@@ -1,24 +1,12 @@
-import React, { Component } from 'react'
+import React from 'react'
 import ArticleLayout from '../../components/article-layout'
 
-// let About = () => (
-//     <ArticleLayout title="About">
-//         <div>
-//             Splitting pages code with <b>react-router</b> and <b>webpack</b>
-//         </div>
-//     </ArticleLayout>
-// );
+let About = () => (
+    <ArticleLayout title="About">
+        <div>
+            Splitting pages code with <b>react-router</b> and <b>webpack</b>
+        </div>
+    </ArticleLayout>
+);
 
-
-class About extends Component {
-    render() {
-        return (
-            <ArticleLayout title="About">
-                <div>
-                    Splitting pages code with <b>react-router</b> and <b>webpack</b>
-                </div>
-            </ArticleLayout>
-        )
-    }
-}
 export default About
--- src/pages/app/index.js
+++ src/pages/app/index.js
@@ -1,15 +1,11 @@
-import React, { Component } from 'react';
+import React from 'react';
 import Layout from '../../components/layout'
 
-class App extends Component {
-  render() {
-    return (
-        <Layout>
-            { this.props.children}
-        </Layout>
-    );
-  }
-}
+let App = (props) => (
+    <Layout>
+        { props.children}
+    </Layout>
+);
 
 App.propTypes = {
     children: React.PropTypes.node
